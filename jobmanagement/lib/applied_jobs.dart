9d444e931e97631551fbd0e4af4d5473a43d0855
Prashant5123 <gordeprashant5123@gmail.com>
2025-09-02 15:00:41 +0530

    Flutter

--- jobmanagement/lib/applied_jobs.dart
+++ jobmanagement/lib/applied_jobs.dart
@@ -33,141 +33,143 @@ class _AppliedJobsState extends State<AppliedJobs> {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          SizedBox(height: 20),
-          Obx(() {
-            return ListView.builder(
-              itemCount: _userDetailsController.userAppliedJobs.length,
-              shrinkWrap: true,
-              // physics: NeverScrollableScrollPhysics(),
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.only(
-                    left: 20,
-                    right: 20,
-                    bottom: 20,
-                  ),
-                  child: Container(
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      border: Border.all(
-                        color: const Color.fromARGB(255, 231, 230, 230),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 20),
+            Obx(() {
+              return ListView.builder(
+                itemCount: _userDetailsController.userAppliedJobs.length,
+                shrinkWrap: true,
+               physics: NeverScrollableScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.only(
+                      left: 20,
+                      right: 20,
+                      bottom: 20,
+                    ),
+                    child: Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(
+                          color: const Color.fromARGB(255, 231, 230, 230),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "${_userDetailsController.userAppliedJobs[index]["title"]}",
+        
+                              style: GoogleFonts.poppins(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              "${_userDetailsController.userAppliedJobs[index]["description"]}",
+        
+                              style: GoogleFonts.poppins(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Container(
+                              decoration: BoxDecoration(
+                                color: Colors.red[50],
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(5.0),
+                                child: Text(
+                                  "Location: ${_userDetailsController.userAppliedJobs[index]["location"]}",
+        
+                                  style: GoogleFonts.poppins(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w400,
+                                    color: Colors.red[400],
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 10),
+        
+                            Container(
+                              decoration: BoxDecoration(
+                                color: Colors.blue[50],
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(5.0),
+                                child: Text(
+                                  "PostedBy: ${_userDetailsController.userAppliedJobs[index]["postedBy"]}",
+        
+                                  style: GoogleFonts.poppins(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w400,
+                                    color: Colors.blue[400],
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 10),
+        
+                            Container(
+                              decoration: BoxDecoration(
+                                color: Colors.pink[50],
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(5.0),
+                                child: Text(
+                                  "Salary: ${_userDetailsController.userAppliedJobs[index]["salary"]}",
+        
+                                  style: GoogleFonts.poppins(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w400,
+                                    color: Colors.pink[400],
+                                  ),
+                                ),
+                              ),
+                            ),
+        
+                            SizedBox(height: 10),
+        
+                            Container(
+                              decoration: BoxDecoration(
+                                color: Colors.green[50],
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(5.0),
+                                child: Text(
+                                  "Posted At: ${_userDetailsController.userAppliedJobs[index]["createdAt"]}",
+        
+                                  style: GoogleFonts.poppins(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w400,
+                                    color: Colors.green[400],
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                          ],
+                        ),
                       ),
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "${_userDetailsController.userAppliedJobs[index]["title"]}",
-
-                            style: GoogleFonts.poppins(
-                              fontSize: 18,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                          SizedBox(height: 10),
-                          Text(
-                            "${_userDetailsController.userAppliedJobs[index]["description"]}",
-
-                            style: GoogleFonts.poppins(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w400,
-                            ),
-                          ),
-                          SizedBox(height: 10),
-                          Container(
-                            decoration: BoxDecoration(
-                              color: Colors.red[50],
-                              borderRadius: BorderRadius.circular(5),
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.all(5.0),
-                              child: Text(
-                                "Location: ${_userDetailsController.userAppliedJobs[index]["location"]}",
-
-                                style: GoogleFonts.poppins(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w400,
-                                  color: Colors.red[400],
-                                ),
-                              ),
-                            ),
-                          ),
-                          SizedBox(height: 10),
-
-                          Container(
-                            decoration: BoxDecoration(
-                              color: Colors.blue[50],
-                              borderRadius: BorderRadius.circular(5),
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.all(5.0),
-                              child: Text(
-                                "PostedBy: ${_userDetailsController.userAppliedJobs[index]["postedBy"]}",
-
-                                style: GoogleFonts.poppins(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w400,
-                                  color: Colors.blue[400],
-                                ),
-                              ),
-                            ),
-                          ),
-                          SizedBox(height: 10),
-
-                          Container(
-                            decoration: BoxDecoration(
-                              color: Colors.pink[50],
-                              borderRadius: BorderRadius.circular(5),
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.all(5.0),
-                              child: Text(
-                                "Salary: ${_userDetailsController.userAppliedJobs[index]["salary"]}",
-
-                                style: GoogleFonts.poppins(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w400,
-                                  color: Colors.pink[400],
-                                ),
-                              ),
-                            ),
-                          ),
-
-                          SizedBox(height: 10),
-
-                          Container(
-                            decoration: BoxDecoration(
-                              color: Colors.green[50],
-                              borderRadius: BorderRadius.circular(5),
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.all(5.0),
-                              child: Text(
-                                "Posted At: ${_userDetailsController.userAppliedJobs[index]["createdAt"]}",
-
-                                style: GoogleFonts.poppins(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w400,
-                                  color: Colors.green[400],
-                                ),
-                              ),
-                            ),
-                          ),
-                          SizedBox(height: 10),
-                        ],
-                      ),
-                    ),
-                  ),
-                );
-              },
-            );
-          }),
-        ],
+                  );
+                },
+              );
+            }),
+          ],
+        ),
       ),
       bottomNavigationBar: UserBottomNavigation().userBottomNavigation(
         context: context,
